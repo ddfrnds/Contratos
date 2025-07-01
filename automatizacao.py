7605c54b085994bf96ff46c828757d2f48e15977
Dainara <dainarafernandas@hotmail.com>
2025-07-01 20:31:00 +0000

    Comandos completos

--- automatizacao.py
+++ automatizacao.py
@@ -1,8 +1,25 @@
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("contrato_rinp_modelo.docx.docx")
+doc = DocxTemplate("contrato_rinp_modelo.docx")
 context = { 
-    "NOME_COMPLETO" : "DAINARA FERNANDA DE SOUSA" }
+    "NOME_COMPLETO" : "DAINARA FERNANDA DE SOUSA",
+     "nacionalidade" : "brasileira",
+     "profissão" : "estagiária",
+     "estado_civil" : "solteira", 
+     "cpf" : "118.616.000-00", 
+     "RG": "019.770.000", 
+     "nome_da_rua" : "Rua Tal", 
+     "n_da_casa" : "175", 
+     "bairro" : "Cidade Nova", 
+     "cidade" : "Gov. Valadares", 
+     "estado" : "MG", 
+     "email" : "dainarafs@gmail.com", 
+     "numero_telefone" : "3398890-1052",
+     "valor" : "12.000,00 ",
+     "valor_extenso" : "vinte mil reais",
+     "forma de pagamento" : "mil vezes no cartão, parcelado 12x",
+     "dia" : "10",
+     "mes" : "maio"}
 
 doc.render(context)
 doc.save("contrato_preenchido.docx")
